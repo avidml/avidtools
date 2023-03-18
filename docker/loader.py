209ad6b47b125001174420376d7f15af6dc67a09
Paolo <paolo@priam.ai>
2023-03-18 09:57:13 +0000

    Added Pydantic validation.

--- docker/loader.py
+++ docker/loader.py
@@ -1,6 +1,15 @@
 from pyArango.connection import *
 import json
 import pathlib
+import pydantic.error_wrappers
+from avidtools.datamodels import report,enums,components,vulnerability
+from pathlib import Path
+import logging
+import argparse
+import json
+
+logging.basicConfig(level=logging.INFO)
+
 
 conn = Connection(username="root", password="root_passwd")
 
@@ -17,20 +26,39 @@ if avid_db.hasCollection("Reports")==False:
     repo_collection = avid_db.createCollection(name="Reports",waitForSync = True)
 
 
-for vuln in pathlib.Path('./vulnerabilities').glob('*.json'):
-    with open(vuln,'r') as json_file:
-        print(vuln)
-        data = json.load(json_file)
-        # add the object key
-        doc = vuln_collection.createDocument(data)
-        doc._key = data['metadata']['vuln_id']
-        doc.save()
+for file in Path('./vulnerabilities').glob('*.json'):
 
-for rep in pathlib.Path('./reports').glob('*.json'):
-    with open(rep,'r') as json_file:
-        print(rep)
-        data = json.load(json_file)
-        # add the object key
-        doc = repo_collection.createDocument(data)
-        doc._key = data['metadata']['report_id']
-        doc.save()
+    with open(file) as f:
+        try:
+            logging.info(file)
+            data = json.load(f)
+            report_obj = vulnerability.Vulnerability(**data)
+
+            # add the object key
+            doc = vuln_collection.createDocument(report_obj.dict())
+            doc._key = data['metadata']['vuln_id']
+            doc.save()
+
+        except pydantic.ValidationError as vae:
+            logging.error(f'File -> {file.as_posix()}')
+            logging.error(vae)
+        except pydantic.error_wrappers.ErrorWrapper as eo:
+            logging.error(eo)
+
+for file in pathlib.Path('./reports').glob('*.json'):
+    with open(file) as f:
+        try:
+            logging.info(file)
+            data = json.load(f)
+            report_obj = report.Report(**data)
+
+            # add the object key
+            doc = repo_collection.createDocument(report_obj.dict())
+            doc._key = data['metadata']['report_id']
+            doc.save()
+
+        except pydantic.ValidationError as vae:
+            logging.error(f'File -> {file.as_posix()}')
+            logging.error(vae)
+        except pydantic.error_wrappers.ErrorWrapper as eo:
+            logging.error(eo)
