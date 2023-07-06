6d95cd57aaccf27a8e588d383a01ffaed8419547
shubhobm <zoom.subha@gmail.com>
2023-07-05 18:59:55 -0700

    feat: started next iteration of datamodel

--- avidtools/datamodels/components.py
+++ avidtools/datamodels/components.py
@@ -35,30 +35,38 @@ class Problemtype(BaseModel):
         
 class Metric(BaseModel):
     """Quantification of the issue in a specific report."""
-    name: str
-    detection_method: Detection
+    name: Optional[str]
+    detection_method: Optional[Detection]
     results: Dict
+
+    class Config: # fields are excluded if None
+        fields = {
+            'name': {'exclude': True},
+            'detection_method': {'exclude': True}
+        }
 
 class Reference(BaseModel):
     """Details for a reference of a report/vulnerability."""
     type: Optional[str]
     label: str
-    url: str # AnyUrl is a better fit, but keeping this because submissions are not standard yet
+    url: str # NOTE: AnyUrl is a better fit, but keeping this because submissions are not standard yet
 
     class Config: # type is excluded if None
         fields = {'type': {'exclude': True}}
 
-class AvidTaxonomy(BaseModel):
+class Taxonomy(BaseModel): # NOTE: rename to store generic taxonomy object
+# class AvidTaxonomy(BaseModel):
     """AVID taxonomy mappings of a report/vulnerability."""
-    vuln_id: Optional[str]
-    risk_domain: List[str]
-    sep_view: List[SepEnum]
-    lifecycle_view: List[LifecycleEnum]
-    taxonomy_version: str
+    vuln_id: Optional[str] # TODO: should move to a new field `Related` that tracks related objects, ref. https://gist.github.com/zoezoezoe/8e499b62d50b155eb721427be41f5fef
+    namespace: str
+    predicate: str
+    value: str
+    taxonomy_version: str # TODO: not needed, should delete
 
     class Config: # vuln_id is excluded if None
         fields = {'vuln_id': {'exclude': True}}
         
 class Impact(BaseModel):
     """Impact information of a report/vulnerability, e.g. different taxonomy mappings, harm and severity scores."""
-    avid: AvidTaxonomy
+    taxonomy: List[Taxonomy]
+    # TODO: add score object to score a report/vuln
