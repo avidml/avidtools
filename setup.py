44ab6b6316bfbcf5521f3a6e2226d877e13fd6a0
shubhobm <zoom.subha@gmail.com>
2025-06-23 20:39:28 -0700

    lock

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import find_packages, setup
   
 setup(
     name='avidtools',
-    version='0.1.3',
+    version='0.2.0',
     description='Developer tools for AVID',
     author='Subho Majumdar',
     author_email='info@avidml.org',
     packages=find_packages(exclude=['.']),
+    python_requires='>=3.12',
     install_requires=[
-        'pydantic',
-        'typing;python_version<"3.5"',
-        'typing_extensions',
-        'datetime',
-        'nvdlib'
+        'pydantic>=2.11.5',
+        'typing-extensions>=4.13.2',
+        'datetime>=5.5',
+        'nvdlib>=0.8.1'
     ],
 )
